6354a8b863913dac91e79df392b6216b7eabfc2e
AdnanAhmed <127620300+AdnanAhmed04@users.noreply.github.com>
2024-01-23 13:01:41 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+
 function generateRandomColor() {
     var red = Math.floor(Math.random() * 256);
     var green = Math.floor(Math.random() * 256);
     var blue = Math.floor(Math.random() * 256);
+    console.log(red)
+    console.log(green)
+    console.log(blue)
 
     var randomColor = `rgb(${red}, ${green}, ${blue})`;
+    console.log(randomColor)
 
     document.getElementById('colorBox').style.backgroundColor = randomColor;
 }
